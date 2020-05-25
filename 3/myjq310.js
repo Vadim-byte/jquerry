73b90659d69e0d2c91c98a7f64c3861e5b610ca9
Vadim-byte <65809621+Vadim-byte@users.noreply.github.com>
2020-05-25 18:09:00 +0500

    Add files via upload

--- 3/myjq310.js
+++ 3/myjq310.js
@@ -1,9 +1,33 @@
-$(function(){
-	
-$(".contatct-active a").click(function(){
-	$(".features-grids").slideUp("slow")
-	}, function(){
-		$(".features-grids").slideDown("slow")
-		// НЕ ЗРОБЛЕНО
-});
-});
+$(".contatct-active").click(
+    function() {
+       $(".contatct-active").html("ua");
+	$(".active").html("Home");
+	$(".page-scroll m2").html("Motocycles");
+	$(".page-scroll m3").html("Sale");
+	$(".page-scroll m4").html("Forum");
+	$(".page-scroll m5").html(" Contacts");
+    }, function() {
+	$(".contatct-active").html("eng"); 
+	$(".active").html("Головна");
+	$(".page-scroll m2").html("Мотоцикли");
+	$(".page-scroll m3").html("Розпродаж");
+	$(".page-scroll m4").html("Форум");
+	$(".page-scroll m5").html(" Контакти");
+    
+	});
+// ЗАВДАННЯ:
+// До меню сторінки додайте останнім пунктом наступний код:
+// <li class="contatct-active m6" class="page-scroll"><a
+// href="#">eng</a></li> (Зроблено)
+//
+// При клікані на цей доданий елемент усі пункти меню повинні стати на
+// англійській мові - Home, Motorcycles, Sale, Forum, Contacts.
+// Сама кнопка повинна змінити текст для перекладу на українську мову -
+// ua.
+// При клікані на кнопку ua текст меню повинен повернутися у початковий
+// стан (українською мовою).
+//
+//
+// Коментар:
+// Схоже завдання на 3.8:
+// (Не розумію, як зробити другий клік по елементу. Начебто логічно зробив, але не працює.)
--- 3/myjq39.js
+++ 3/myjq39.js
@@ -1,7 +1,7 @@
 $(function(){
 	
 $(".blog-post:first").hover(function(){
- $(".categorie:first").show(1000)
+ $(".categorie:first").show(1000);
  }, function(){
  $(".categorie:first").hide(1000);
 });
@@ -9,7 +9,7 @@ $(".blog-post:first").hover(function(){
 
 	
 $(".blog-post:second").hover(function(){
- $(".categorie:second").show(1000)
+ $(".categorie:second").show(1000);
  }, function(){
  $(".categorie:second").hide(1000);
 });
@@ -17,7 +17,7 @@ $(".blog-post:second").hover(function(){
 
 	
 $(".blog-post:third").hover(function(){
- $(".categorie:third").show(1000)
+ $(".categorie:third").show(1000);
  }, function(){
  $(".categorie:third").hide(1000);
 });
@@ -25,10 +25,18 @@ $(".blog-post:third").hover(function(){
 
 	
 $(".blog-post:fourth").hover(function(){
- $(".categorie:fourth").show(1000)
+ $(".categorie:fourth").show(1000);
  }, function(){
  $(".categorie:fourth").hide(1000);
 });
 });
 
-//не працює
+// ЗАВДАННЯ
+// При наведені мишею на порядковий номер мотоциклу б/у потрібно у саме
+// цього мотоциклу відобразити вартість за допомогою методу зміни
+// прозорості елементу. Коли миша покидає елемент з номером мотоциклу –
+// його вартість зникає методом зміни прозорості.
+//
+//
+//Коментар:
+//Якщо лишити лише першу функцію - усе працює, як добавляю нову - не працює. Скоріше всього я взяв не правильний тег, але не знаю який потрібно.
